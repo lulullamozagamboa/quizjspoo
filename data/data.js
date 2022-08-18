f80205eb3ebeb3d5e4efe5ff039081651f64b2b6
lulu.llamozagaboa <luciallamozagamboa>
2022-08-17 22:51:26 -0300

    se agrega el proyect

---
+++ data/data.js
@@ -0,0 +1,35 @@
+const data = [{
+    "question":"cual de las siguientes opciones no es un editor de codigo",
+    choices:[
+        "vim",
+        "vscode",
+        "emacs",
+        "word",
+    ]
+    answer:"word"
+    },
+    
+    {
+        "question":"que lenguaje no es orientado a objetos",
+        choices:[
+            "java",
+            "haskell",
+            "C++",
+            "Python",
+        ]
+        answer:"haskell"
+        },
+
+        {
+            "question":"que lenguaje no sirve paa estilizar sitios web",
+            choices:[
+                "stylus",
+                "less",
+                "sass",
+                "postCSS",
+                "NextCSS",
+
+            ]
+            answer:"postCSS"
+            },
+]
